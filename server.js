9c9b880826f7ff23b85b81cbddfcbe1b90130d73
Gelly-Tr33s <nfgel007@gmail.com>
2025-12-04 23:00:29 +0800

    added secure features

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const expect = require('chai');
 const socket = require('socket.io');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner.js');
@@ -15,6 +16,16 @@ app.use('/assets', express.static(process.cwd() + '/assets'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Security measures
+app.use(helmet.noSniff());
+app.use(helmet.xssFilter());
+app.use(helmet.dnsPreFetchControl());
+app.use(helmet.noCache());
+app.use((req, res, next) => {
+  res.setHeader('X-Powered-By', 'PHP 7.4.3');
+  next();
+});
 
 //For FCC testing purposes and enables user to connect from outside the hosting platform
 app.use(cors({origin: '*'})); 
